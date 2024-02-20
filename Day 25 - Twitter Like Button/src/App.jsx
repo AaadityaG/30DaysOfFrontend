54d677463bb5f54010ea648f1f35eab1c821b3b2
Aditya <helloadig@gmail.com>
2024-02-20 16:40:17 +0530

    Twitter Like Button Animation

---
+++ Day 25 - Twitter Like Button/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [likeCount, setLikeCount] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  const toggleDisplay = () => {
+    if(likeCount === 0){
+      setLikeCount(likeCount+1);
+      setLiked(true);
+    } else {
+      setLikeCount(likeCount-1);
+      setLiked(false);
+    }
+  };
+  return (
+    <div className='App'>
+      <div className='heart-bg'>
+        <div className={`heart-icon ${liked ? 'liked' : ''}`} onClick={toggleDisplay}></div>
+        </div>
+        <div className='likes-amount'>
+          {likeCount}
+        </div>
+    </div>
+  );
+} 
+
+export default App;
