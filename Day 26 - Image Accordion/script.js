6622027a220668d394b94ea9c7a77ac688023557
Aditya <helloadig@gmail.com>
2024-02-21 19:27:28 +0530

    Image Accordion

---
+++ Day 26 - Image Accordion/script.js
@@ -0,0 +1,54 @@
+const items = document.querySelectorAll(".item");
+let imageURLs = [
+  "https://images.unsplash.com/photo-1682687220363-35e4621ed990?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8",
+  "https://images.unsplash.com/photo-1707343848723-bd87dea7b118?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1631473126856-d1fdf3bf9c4f?q=80&w=435&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1553440569-bcc63803a83d?q=80&w=1425&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=1575&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+//initially empty
+let deviceType = "";
+let events = {
+  mouse: {
+    start: "mouseover",
+    end: "mouseout",
+  },
+  touch: {
+    start: "touchstart",
+    end: "touchend",
+  },
+};
+
+const isTouchDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (e) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+isTouchDevice();
+
+items.forEach((item, index) => {
+  let img = document.createElement("img");
+  img.setAttribute("src", imageURLs[index]);
+  img.style.width = "100%";
+  img.style.height = "100%";
+  img.style.objectFit = "cover";
+  item.appendChild(img);
+
+  //Initial CSS properties for all items
+  item.style.flex = "1";
+  item.style.transition = "flex 0.8s ease";
+
+  item.addEventListener(events[deviceType].start, () => {
+    item.style.flex = "9"; //Expand the item
+  });
+  item.addEventListener(events[deviceType].end, () => {
+    item.style.flex = "1"; //Contract the item
+  });
+});
